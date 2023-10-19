ab45172fa5394c8e39f04583b15e5a5869e854ff
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-10-19 18:30:36 +0400

    get and post operation done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors());
@@ -28,6 +28,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const carCollection = client.db('carDB').collection('cars');
+
+    app.get('/cars', async(req, res)=>{
+        const cursor = carCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+        
+    })
+    //experiment
+    // app.get('/cars/brand/Honda', async(req, res)=>{
+    //     const cursor = carCollection.find('Honda');
+    //     const result = await cursor.toArray();
+    //     res.send(result);
+        
+    // })
+
+    app.post('/cars', async(req, res)=>{
+        const newCar = req.body;
+        console.log(newCar)
+        const result = await carCollection.insertOne(newCar);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
